38760286251e51f5cb3b17e0c7d26e3f65d8796b
ahkam-9408 <it18098174@my.sliit.lk>
2021-10-01 22:12:17 +0530

    initial prject

---
+++ src/main/java/com/ssd/ssdauthproject/SsdAuthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ssd.ssdauthproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdAuthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdAuthProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssd/ssdauthproject/SsdAuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssd.ssdauthproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdAuthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
