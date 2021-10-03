aa11717d52f8b73957ec2ecb08be69492864536f
ahkam-9408 <it18098174@my.sliit.lk>
2021-10-03 10:20:44 +0530

    git main details added

--- src/main/java/com/ssd/ssdauthproject/controller/MainController.java
+++ src/main/java/com/ssd/ssdauthproject/controller/MainController.java
@@ -1,12 +1,58 @@
 package com.ssd.ssdauthproject.controller;
 
+import com.ssd.ssdauthproject.credentials.Credential;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class MainController {
+    final String facebookLoginURL = "https://github.com/login/oauth/authorize?" +
+//            "response_type=code" +
+            "&client_id=" + Credential.clientId +
+            "&redirect_uri=" + Credential.redirectURL +
+            "&scope="+ Credential.scope +
+            "&state=" + Credential.state;
+
+    static String getToken = "";
+
+    static String code = "";
+
+//    @Autowired
+//    private PersonService personService;
+//
+//    @Autowired
+//    private PersonRepository personRepository;
+
     @GetMapping("/")
     public String loadIndexPage() throws Exception{
-        return "redirect:/index.html";
+        return code.equals("") ? "redirect:/index.html" : "redirect:/github.html";
     }
+
+    @RequestMapping("/facebook-login")
+    public ModelAndView facebookLogin() throws Exception{
+        return new ModelAndView("redirect:" + facebookLoginURL);
+    }
+
+    @GetMapping("/github")
+    public String loadGithubPage(HttpServletRequest request) throws Exception{
+        code = request.getParameter("code");
+        return code.equals("") ? "redirect:/" : "redirect:/github.html" ;
+    }
+
+//    @RequestMapping(value = "/persistPerson", method = RequestMethod.POST)
+//    public ResponseEntity< String > persistPerson(@RequestBody PersonDTO person) {
+//        if (personService.isValid(person)) {
+//            personRepository.persist(person);
+//            return ResponseEntity.status(HttpStatus.CREATED).build();
+//        }
+//        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
+//    }
 }
---
+++ src/main/java/com/ssd/ssdauthproject/credentials/Credential.java
@@ -0,0 +1,10 @@
+package com.ssd.ssdauthproject.credentials;
+
+public class Credential {
+
+    final static public String clientId = "5b8a23cfc2df318b5cd1";
+    final static public String redirectURL = "http%3A%2F%2Flocalhost%3A8081%2Fgithub%2F";
+    final static public String scope = "read:public_key%20 admin:org";
+    final static public String state = "auth6845";
+
+}
