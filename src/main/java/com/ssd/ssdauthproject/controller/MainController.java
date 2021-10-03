fe947836cbed3d3cef55cda12f66368a521a89cc
ahkam-9408 <it18098174@my.sliit.lk>
2021-10-03 18:11:48 +0530

    code refectored

--- src/main/java/com/ssd/ssdauthproject/controller/MainController.java
+++ src/main/java/com/ssd/ssdauthproject/controller/MainController.java
@@ -1,15 +1,32 @@
 package com.ssd.ssdauthproject.controller;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.ssd.ssdauthproject.credentials.Credential;
 import com.ssd.ssdauthproject.model.CreateRepo;
 import com.ssd.ssdauthproject.utils.Constants;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.HashMap;
 
@@ -25,17 +42,100 @@ public class MainController {
 
     static String token = "";
 
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @Autowired
+    com.ssd.ssdauthproject.service.UploadService uploadService;
+
+    @Autowired
+    com.ssd.ssdauthproject.service.AuthService authService;
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @Value("${google.secret.key.path}")
+    private String pathDriveSecretKey;
+
+    @Value("${google.credentials.folder.path}")
+    private String pathCredentialFolder;
+
+    @Value("${google.oauth.callback.uri}")
+    private String CALLBACK_URI;
+
+    private GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow;
+
+    @PostConstruct
+    public void init() throws Exception {
+
+        GoogleClientSecrets secrets = GoogleClientSecrets.load(com.ssd.assignment.oauth.demo.util.Constants.JSON_FACTORY,
+                new InputStreamReader(resourceLoader.getResource(pathDriveSecretKey).getInputStream()));
+        googleAuthorizationCodeFlow = new GoogleAuthorizationCodeFlow.Builder(com.ssd.assignment.oauth.demo.util.Constants.HTTP_TRANSPORT, com.ssd.assignment.oauth.demo.util.Constants.JSON_FACTORY, secrets, com.ssd.assignment.oauth.demo.util.Constants.SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(resourceLoader.getResource(pathCredentialFolder).getFile())).build();
+    }
+
+    @GetMapping("/uploader")
+    public String getHomePage() throws IOException {
+
+        return isAuthenticated() ? "redirect:/uploader.html" : "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String showHomePage() throws Exception {
+        return "redirect:/index.html";
+    }
+
+    @GetMapping("/oauth/callback")
+    public String saveAuthorizationCode(HttpServletRequest request) throws Exception {
+
+        String code = request.getParameter("code");
+        logger.info("Code - {} ", code);
+        if (code != null) {
+            GoogleTokenResponse response = googleAuthorizationCodeFlow.newTokenRequest(code).setRedirectUri(CALLBACK_URI).execute();
+            googleAuthorizationCodeFlow.createAndStoreCredential(response, com.ssd.assignment.oauth.demo.util.Constants.USER_IDENTIFIER_KEY);
+            return "redirect:/uploader";
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/logout")
+    public String logout() throws Exception {
+        Resource credentialsFolder = resourceLoader.getResource(pathCredentialFolder);
+        FileUtils.cleanDirectory(credentialsFolder.getFile());
+        logger.info("Logged Out");
+        return "redirect:/";
+    }
+
+    @GetMapping("/signingoogle")
+    public void doGoogleSignIn(HttpServletResponse response) throws Exception {
+        GoogleAuthorizationCodeRequestUrl url = googleAuthorizationCodeFlow.newAuthorizationUrl();
+        String redirectURL = url.setRedirectUri(CALLBACK_URI).setAccessType("offline").build();
+        logger.info("Redirect URL {} ", redirectURL);
+        response.sendRedirect(redirectURL);
+    }
+
+    @PostMapping("/upload")
+    public String uploadFileToDrive(@RequestParam("file") MultipartFile multipartFile) throws Exception {
+
+        com.google.api.client.auth.oauth2.Credential cred = googleAuthorizationCodeFlow.loadCredential(com.ssd.assignment.oauth.demo.util.Constants.USER_IDENTIFIER_KEY);
+        String id = uploadService.uploadToDrive(cred, multipartFile);
+        logger.info("Uploaded file id - {} ", id);
+        return "redirect:/uploader";
+
+    }
+
+    private boolean isAuthenticated() throws IOException {
+
+        com.google.api.client.auth.oauth2.Credential credential = googleAuthorizationCodeFlow.loadCredential(com.ssd.assignment.oauth.demo.util.Constants.USER_IDENTIFIER_KEY);
+        return authService.authenticateCredentials(credential);
+
+    }
+
     private String getTokenURL(String code){
         return "https://github.com/login/oauth/access_token?" +
                 "client_id="+ Credential.clientId +
                 "&client_secret=" + Credential.client_secret +
                 "&code="+code +
                 "&redirect_uri=" + Constants.REDIRECT_URL;
-    }
-
-    @GetMapping("/")
-    public String loadIndexPage() throws Exception{
-        return token.equals("") ? "redirect:/index.html" : "redirect:/github.html";
     }
 
     @RequestMapping("/github-login")
@@ -54,7 +154,7 @@ public class MainController {
 
         HashMap map = new HashMap<>();
         map.put("name",createRepo.getReponame());
-        map.put("private",createRepo.getStatus() == "private" ? true : false);
+        map.put("private",createRepo.getStatus().equals("private"));
         map.put("description",createRepo.getDescription());
 
         HttpEntity httpHeader = new HttpEntity<>(map,headers);
--- src/main/java/com/ssd/ssdauthproject/utils/Constants.java
+++ src/main/java/com/ssd/ssdauthproject/utils/Constants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Constants {
 
-    public static final String REDIRECT_URL = "http%3A%2F%2Flocalhost%3A8081%2Fgithub%2F";
+    public static final String REDIRECT_URL = "http%3A%2F%2Flocalhost%3A8080%2Fgithub%2F";
     final static public String SCOPE = "repo";
     final static public String STATE = "auth6845";
 }
